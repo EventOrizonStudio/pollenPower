77b4bf9e929029ea84265ab6727c8fa0125fd0c3
unknown <theludovyc@gmail.com>
2019-05-12 14:46:24 +0200

    Grand Guy

--- Script/Guy.gd
+++ Script/Guy.gd
@@ -8,6 +8,24 @@ extends Node
 func _ready():
 	pass # Replace with function body.
 
+func setPoint(i:int):
+	$CenterContainer2/Label2.text = str(i)
+	
+func setLife(f:float):
+	$CenterContainer2/Label2/TextureProgress.value = f
+	
+func setText(s:String):
+	$Label.text = s
+	
+func setTextVisible(b:bool):
+	$Label.visible = b
+	
+func startTimer():
+	$Timer.start()
+	
+func stopTimer():
+	$Timer.stop()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
