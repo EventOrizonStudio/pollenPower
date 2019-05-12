faf937fe9df6e4ebe84bafaf66fda72c04a7fc06
alto971 <mafabrikjeux@gmail.com>
2019-05-12 12:19:09 +0200

    change for space input

--- Script/Nabeille.gd
+++ Script/Nabeille.gd
@@ -3,6 +3,7 @@ extends Area2D
 # Declare member variables here. Examples:
 export(int, 0, 10) var gravityIntensity = 5
 export(int, 0, 10) var jumpSpeed = 10
+export(int, 0, 10) var horizontalSpeed = 5
 export(int, 0, 10) var maxVel = 5
 onready var vel = Vector2()
 # var b = "text"
@@ -13,13 +14,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("ui_up"):
+	if Input.is_action_just_pressed("ui_select"):
 		vel.y -= jumpSpeed
 		vel.y = clamp(vel.y,-maxVel,maxVel)
 	vel.y += gravityIntensity * delta
-	print(vel)
-	#vel.y = clamp(vel.y,0,maxVel)
+	vel.x = horizontalSpeed
 	position += vel
-	print(vel)
+	
+	  
 	
 
