4af5a4a86e3ad632c4a2bf2754675520655e3ec2
alto971 <mafabrikjeux@gmail.com>
2019-05-12 12:02:18 +0200

    added jump

--- Script/Nabeille.gd
+++ Script/Nabeille.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
 # Declare member variables here. Examples:
-var gravityIntensity = 11
+export(int, 0, 10) var gravityIntensity = 5
+export(int, 0, 10) var jumpSpeed = 10
+export(int, 0, 10) var maxVel = 5
 onready var vel = Vector2()
 # var b = "text"
 
@@ -11,5 +13,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_just_pressed("ui_up"):
+		vel.y -= jumpSpeed
+		vel.y = clamp(vel.y,-maxVel,maxVel)
 	vel.y += gravityIntensity * delta
+	print(vel)
+	#vel.y = clamp(vel.y,0,maxVel)
 	position += vel
+	print(vel)
+	
+
