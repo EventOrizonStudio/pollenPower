f9e1177216b2e6baf211836de86fed5c8b899578
Cevantime <thibaulttruffert@hotmailcom>
2019-05-12 12:07:41 +0200

    Add pollen pop

---
+++ Scene/PollenPop.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+export(PackedScene) var pollen_scene = preload("Pollen.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_PopTimer_timeout():
+	pop_pollen()
+	
+func pop_pollen():
+	$PopPath/PopFollow.unit_offset = randf()
+	var pop_position = $PopPath/PopFollow.position
+	print(pop_position)
+	var pollen = pollen_scene.instance();
+	pollen.position = pop_position
+	add_child(pollen)
+	
