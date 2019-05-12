92b69fad1e9468f36633c72f6e730185133a92fd
alto971 <mafabrikjeux@gmail.com>
2019-05-12 11:28:00 +0200

    add gravity

--- Script/Nabeille.gd
+++ Script/Nabeille.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 # Declare member variables here. Examples:
-# var a = 2
+var gravityIntensity = 11
+onready var vel = Vector2()
 # var b = "text"
 
 # Called when the node enters the scene tree for the first time.
@@ -9,5 +10,6 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	vel.y += gravityIntensity * delta
+	position += vel
